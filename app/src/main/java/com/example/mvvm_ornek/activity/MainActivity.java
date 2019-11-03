7ea4f2618205f5e7532ae7caffe56fc99ba3033d
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2019-11-03 15:54:06 +0300

    v1

---
+++ app/src/main/java/com/example/mvvm_ornek/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mvvm_ornek.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mvvm_ornek.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
